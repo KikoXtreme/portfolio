e8357211ced521c364d6bedcb5c1a3c303410bcf
Kiril Valkov <kiril_dgr@abv.bg>
2024-10-10 16:43:14 +0300

    initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import '../../css/footer.css'
+
+const Footer = () => {
+    return (
+        <footer>&copy; 2024 Kiril Valkov - All rights reserved.</footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,15 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+
+const Portfolio = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default Portfolio;
