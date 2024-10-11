e55ebaf73bbe8ccddad1e819a6dcf9bed07c188f
Kiril Valkov <kiril_dgr@abv.bg>
2024-10-11 17:01:17 +0300

    about update

---
+++ src/components/About/About.tsx
@@ -0,0 +1,17 @@
+import '../../css/about.css'
+
+const About = () => {
+    return (
+        <section id="about" className="about">
+            <h3>
+                About
+            </h3>
+            <p>
+                Hello there! My name is Kiril Valkov and I am a front-end web developer. My journey into the world of Web development began with intensive specialized courses at Softuni, where I took the JS Web Development path. Throughout the courses, I consistently achieved excellent scores. As a result, I have become proficient in a range of technologies, including JavaScript, TypeScript, React, HTML & CSS, Material UI, Redux Toolkit, Angular, Node.js, MongoDB and more.
+                {/* <div id="skills"></div> */}
+            </p>
+        </section>
+    )
+}
+
+export default About;
