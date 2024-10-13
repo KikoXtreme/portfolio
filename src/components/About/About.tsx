27b6c4783a5cb2bd3851f91f0fec261f1c5ccee2
Kiril Valkov <kiril_dgr@abv.bg>
2024-10-13 17:49:17 +0300

    about update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,12 +3,13 @@ import '../../css/about.css'
 const About = () => {
     return (
         <section id="about" className="about">
-            <h3>
-                About
-            </h3>
+            <h2>About</h2>
+            <h3>My name is Kiril Valkov and I am a front-end web developer.</h3>
             <p>
-                Hello there! My name is Kiril Valkov and I am a front-end web developer. My journey into the world of Web development began with intensive specialized courses at Softuni, where I took the JS Web Development path. Throughout the courses, I consistently achieved excellent scores. As a result, I have become proficient in a range of technologies, including JavaScript, TypeScript, React, HTML & CSS, Material UI, Redux Toolkit, Angular, Node.js, MongoDB and more.
-                {/* <div id="skills"></div> */}
+                My journey into the world of Web development began with intensive specialized courses at Softuni, where I took the JS Web Development path. Throughout the courses, I consistently achieved excellent scores. As a result, I have become proficient in a range of technologies, including JavaScript, TypeScript, React, HTML & CSS, Material UI, Redux Toolkit, Angular, Node.js, MongoDB and more.
+            </p>
+            <p>
+                Later my path get me to a job - first as a Junior Front-End Developer and later as a React Developer. I have perfected and improved my skills even more. I have gained knowledge with Figma, Jira, Monday.com, Bootstrap, Preprocessors, especially SCSS where heavily used for styling my working Landing pages and components. I am still getting to know the programming world and still eager to get more proficient and to excell at my work.
             </p>
         </section>
     )
