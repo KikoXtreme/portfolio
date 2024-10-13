ff1dade5b695450dbd9a4795fc6a83b0fe86137a
Kiril Valkov <kiril_dgr@abv.bg>
2024-10-13 17:26:21 +0300

    work update

---
+++ src/components/Work/Work.tsx
@@ -0,0 +1,46 @@
+import '../../css/work.css';
+
+const Work = () => {
+    return (
+        <section id="work" className="work">
+            <h2>Work Experience</h2>
+            <div className="work-card">
+                <h3>React Developer</h3>
+                <h4>Tiebreak</h4>
+                <h5>Oct 2023 - Present</h5>
+            </div>
+            <div className="work-card">
+                <h3>Junior Front-End Developer</h3>
+                <h4>NexGen</h4>
+                <h5>Jan 2023 - Jul 2023</h5>
+            </div>
+            <div className="work-card">
+                <h3>Purchases and Sales Manager</h3>
+                <h4>“Chateau Valon” Supermarket</h4>
+                <h5>May 2019 - Sep 2022</h5>
+            </div>
+            <div className="work-card">
+                <h3>Sales Manager</h3>
+                <h4>“Home Master” Store</h4>
+                <h5>June 2015 - Oct 2018</h5>
+            </div>
+            <div className="work-card">
+                <h3>Technical Support Specialist</h3>
+                <h4>C3i Europe</h4>
+                <h5>Nov 2009 - Jan 2013</h5>
+            </div>
+            <div className="work-card">
+                <h3>Shop assistant, Cashier</h3>
+                <h4>Abercrombie and Fitch</h4>
+                <h5>Jun 2007 - Oct 2007</h5>
+            </div>
+            <div className="work-card">
+                <h3>Waiter, Busser</h3>
+                <h4>Sarentos Restaurant</h4>
+                <h5>Jun 2007 - Oct 2007</h5>
+            </div>
+        </section>
+    )
+}
+
+export default Work;
