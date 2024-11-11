62dfd681102741e6659e4223c8cf6bbbeda64352
Kiril Valkov <kiril_dgr@abv.bg>
2024-11-11 23:13:09 +0200

    contacts update

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -4,22 +4,34 @@ const Contacts = () => {
     return (
         <section id="contacts" className="contacts">
             <h2>Contacts</h2>
+            <a href="https://github.com/KikoXtreme" target="_blank" rel="nooper noreferrer">
+                <div className="card-wrapper">
+                    <img src="/portfolio/assets/github.png" alt="git icon" />
+                    <p>GitHub</p>
+                </div>
+            </a>
             <a href="https://www.linkedin.com/in/kiril-valkov-26a75a37/" target="_blank" rel="nooper noreferrer">
                 <div className="card-wrapper">
-                    <img src="/portfolio/assets/linkedin.png" alt="Mail Icon" />
+                    <img src="/portfolio/assets/linkedin.png" alt="linkedin icon" />
                     <p>Kiril Valkov</p>
                 </div>
             </a>
             <div className="card-wrapper">
-                <img src="/portfolio/assets/phone.png" alt="Mail Icon" />
+                <img src="/portfolio/assets/phone.png" alt="phone icon" />
                 <div>
                     <p>+359 878 801590</p>
                 </div>
             </div>
             <div className="card-wrapper">
-                <img src="/portfolio/assets/mail.png" alt="Mail Icon" />
+                <img src="/portfolio/assets/mail.png" alt="email icon" />
                 <div>
                     <p>valkov.kiko@gmail.com</p>
+                    {/* <p>kiril_dgr@abv.bg</p> */}
+                </div>
+            </div>
+            <div className="card-wrapper">
+                <img src="/portfolio/assets/mail.png" alt="email icon" />
+                <div>
                     <p>kiril_dgr@abv.bg</p>
                 </div>
             </div>
