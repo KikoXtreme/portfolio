fa8e7c36917483ab174aed1996a9d0134cc5000f
Kiril Valkov <kiril_dgr@abv.bg>
2024-11-13 21:38:48 +0200

    certificates update

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -4,16 +4,16 @@ const Certificates = () => {
     return (
         <section id="certificates" className="certificates">
             <h2>Certificates</h2>
-            <article className="links-wrapper">
+            <div className="links">
                 <a href="https://softuni.bg/certificates/details/104949/2e815782" target="_blank" rel="nooper noreferrer">Programming Basics</a>
-                <a href="https://softuni.bg/certificates/details/111154/337159e2" target="_blank" rel="nooper noreferrer" >Programming Fundamentals with JS</a>
+                <a href="https://softuni.bg/certificates/details/111154/337159e2" target="_blank" rel="nooper noreferrer">Programming Fundamentals JS</a>
                 <a href="https://softuni.bg/certificates/details/121130/056116e4" target="_blank" rel="nooper noreferrer">JS Advanced</a>
                 <a href="https://softuni.bg/certificates/details/120894/afa55f2f" target="_blank" rel="nooper noreferrer">JS Applications</a>
                 <a href="https://softuni.bg/certificates/details/132060/e4a1e952" target="_blank" rel="nooper noreferrer">Angular</a>
                 <a href="https://softuni.bg/certificates/details/136967/d77a8848" target="_blank" rel="nooper noreferrer">HTML & CSS</a>
                 <a href="https://softuni.bg/certificates/details/142318/de299408" target="_blank" rel="nooper noreferrer">JS Back-End</a>
                 <a href="https://softuni.bg/certificates/details/141693/3722928b" target="_blank" rel="nooper noreferrer">ReactJS</a>
-            </article>
+            </div>
         </section>
     )
 }
